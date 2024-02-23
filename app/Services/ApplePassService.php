ce93fe64911b219e69354f531b7f9c173d7b5477
hmimeee <hmimeee@gmail.com>
2024-02-23 19:43:53 +0600

    Added the feature

--- app/Services/ApplePassService.php
+++ app/Services/ApplePassService.php
@@ -2,21 +2,13 @@
 
 namespace App\Services;
 
-use Apple\Passbook\PassFactory;
-use Apple\Passbook\PassFactoryInterface;
-use Apple\Passbook\PassSigner;
-use Apple\Passbook\PassSignerInterface;
+use PKPass\PKPass;
 
 class ApplePassService
 {
-    private PassFactoryInterface $passFactory;
-    private PassSignerInterface $passSigner;
-
     public function __construct()
     {
-        // Initialize PassFactory and PassSigner
-        $this->passFactory = new PassFactory();
-        $this->passSigner = new PassSigner();
+        //
     }
 
     /**
@@ -33,6 +25,63 @@ class ApplePassService
      */
     public function createPass(string $passTypeIdentifier, string $serialNumber, string $teamIdentifier, array $data, string $certificatePath, string $certificatePassword)
     {
+        $pass = new PKPass(public_path('Certificates.p12'), '123456');
+        $data = [
+            'description' => 'Demo pass',
+            'formatVersion' => 1,
+            'organizationName' => 'Flight Express',
+            'passTypeIdentifier' => 'pass.com.scholica.flights', // Change this!
+            'serialNumber' => '12345678',
+            'teamIdentifier' => 'KN44X8ZLNC', // Change this!
+            'boardingPass' => [
+                'primaryFields' => [
+                    [
+                        'key' => 'origin',
+                        'label' => 'San Francisco',
+                        'value' => 'SFO',
+                    ],
+                    [
+                        'key' => 'destination',
+                        'label' => 'London',
+                        'value' => 'LHR',
+                    ],
+                ],
+                'secondaryFields' => [
+                    [
+                        'key' => 'gate',
+                        'label' => 'Gate',
+                        'value' => 'F12',
+                    ],
+                    [
+                        'key' => 'date',
+                        'label' => 'Departure date',
+                        'value' => '07/11/2012 10:22',
+                    ],
+                ],
+                'backFields' => [
+                    [
+                        'key' => 'passenger-name',
+                        'label' => 'Passenger',
+                        'value' => 'John Appleseed',
+                    ],
+                ],
+                'transitType' => 'PKTransitTypeAir',
+            ],
+            'barcode' => [
+                'format' => 'PKBarcodeFormatQR',
+                'message' => 'Flight-GateF12-ID6643679AH7B',
+                'messageEncoding' => 'iso-8859-1',
+            ],
+            'backgroundColor' => 'rgb(32,110,247)',
+            'logoText' => 'Flight info',
+            'relevantDate' => date('Y-m-d\TH:i:sP')
+        ];
+        $pass->setData($data);
+        $pass->addFile(public_path('images/icon.png'));
+        $pass->addFile(public_path('images/avatars/2.png'));
+        $pass->addFile(public_path('images/avatars/3.png'));
+        return $pass->create(true);
+
         // Create a pass using PassFactory
         $pass = $this->passFactory->createPass($passTypeIdentifier, $serialNumber, $teamIdentifier, $data);
 
